7f2678ba619f1acb88a453c51dd39e8ec3096214
jennymb <j.morata.2017@alumnos.urjc.es>
2018-03-27 23:50:10 +0200

    (Esto es solo una prueba para ver si se me actualiza bien GitHub)

--- openfda-1/programa-2.py
+++ openfda-1/programa-2.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import http.client
 import json
 
@@ -16,6 +15,3 @@ reponse = json.loads(repos_raw)
 
 for i in range (len (repos['results'])):
     print ('El identificador del medicamento',i+1,'es'+repos['results'][i]['id'])
-=======
-
->>>>>>> origin/master
