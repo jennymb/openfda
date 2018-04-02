625140c7064bd1eb489d2a23943937e66cb0ca28
jennymb <j.morata.2017@alumnos.urjc.es>
2018-04-02 12:00:11 +0200

    Comentado.

--- openfda-1/programa-2.py
+++ openfda-1/programa-2.py
@@ -4,14 +4,22 @@ import json
 headers = {'User-Agent': 'http-client'}
 
 conn = http.client.HTTPSConnection("api.fda.gov")
-conn.request("GET", "/drug/label.json?limit=10", None, headers)
+conn.request("GET", "/drug/label.json?limit=10", None, headers) #en este caso,como queremos la información de
+                                                                # 10 pacientes, ponemos limit=10.
 r1 = conn.getresponse()
 
 print(r1.status, r1.reason)
-repos_raw = r1.read().decode("utf-8")
+r2 = r1.read().decode("utf-8")
 conn.close()
 
-response = json.loads(repos_raw)
+response = json.loads(r2)
 
 for i in range (len (response['results'])):
     print ('El identificador del medicamento',i+1,'es'+response['results'][i]['id'])
+
+# el único cambio frente al programa 1, es que realizamos un bucle for iterando por cada posición de los elementos
+# (en este caso iterando sobre la información de cada medicamento) y obteniendo e imprimiendo por pantalla
+# para cada medicamento (i + 1 porque el iterador i comienza en 0, por tanto el número del medicamento corresponde a
+# un número más) el identificador del mismo.
+# comprobamos que para los 10 primeros está definido su identificador, por lo que para este documento no
+# hace falta el uso de excepciones, ya que no nos dará un KeyError.
